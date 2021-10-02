c2f7d91b1494502cd03c8f2a4264fbdafdd8714a
Bernkastel07th <koki.sato.work@gmail.com>
2021-10-02 21:35:19 +0900

    setup prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,25 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": proccess.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": proccess.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        tabWidth: 2,
-        singleQuote: false,
-        semi: true,
-      },
-    ],
+    'no-console': () => {
+      if (typeof process.env.NODE_ENV === 'production') {
+        return 'warn'
+      }
+
+      return 'off'
+    },
+    'no-debugger': () => {
+      if (typeof process.env.NODE_ENV === 'production') {
+        return 'warn'
+      }
+
+      return 'off'
+    },
+    'prettier/prettier': ['error'],
   },
-};
+}
